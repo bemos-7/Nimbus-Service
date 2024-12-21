e7f162c0423d476902754d09f3875696ec20c743
bemos <bemo.0@mail.ru>
2024-12-21 20:49:28 +0300

    - add constants

--- android/app/src/main/java/com/bemos/nimbus/shared/Constants.kt
+++ android/app/src/main/java/com/bemos/nimbus/shared/Constants.kt
@@ -5,9 +5,18 @@ object Constants {
     private const val SERVER_IP = ""
     private const val VERSION = "v1"
     private const val USERS = "users"
+    private const val FILES = "files"
 
+    const val USER_FOLDER = "user_folder"
+    const val FILE_NAME = "filename"
     const val BASE_SERVICE_URL = "http://$SERVER_IP/"
     const val USERS_REGISTER = "$USERS/api/$VERSION/$USERS/register"
+    const val SAVE_FILES = "$FILES/api/$VERSION/$FILES/save_file"
+    const val LIST_FILES = "$FILES/api/$VERSION/$FILES/list_files"
+    const val DOWNLOAD_FILE = "$FILES/api/$VERSION/$FILES/file"
 
     const val NAV_ON_BOARD = "onBoard"
+    const val NAV_LIST_OF_FILES = "listOfFiles"
+
+    const val SHARED_PREF_KEY = "KEY"
 }
