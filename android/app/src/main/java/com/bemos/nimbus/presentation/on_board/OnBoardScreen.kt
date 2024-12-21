b5fb7cfb0c8e6c07efc0e57de5b9d1d38d7e872b
bemos <bemo.0@mail.ru>
2024-12-21 20:54:18 +0300

    - add new logic in start screen

--- android/app/src/main/java/com/bemos/nimbus/presentation/on_board/OnBoardScreen.kt
+++ android/app/src/main/java/com/bemos/nimbus/presentation/on_board/OnBoardScreen.kt
@@ -5,19 +5,28 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import com.bemos.nimbus.presentation.on_board.vm.OnBoardViewModel
 import com.bemos.nimbus.presentation.on_board.vm.factory.OnBoardViewModelFactory
+import com.bemos.nimbus.shared.Constants.NAV_LIST_OF_FILES
 
 @Composable
 fun OnBoardScreen(
     modifier: Modifier = Modifier,
+    navController: NavController,
     onBoardViewModelFactory: OnBoardViewModelFactory
 ) {
-   val viewModel = viewModel<OnBoardViewModel>(
-       factory = onBoardViewModelFactory
-   )
+    val viewModel = viewModel<OnBoardViewModel>(
+        factory = onBoardViewModelFactory
+    )
 
     val key by viewModel.key.collectAsState()
+    val keyShared by viewModel.keyShared.collectAsState()
+
+    if (keyShared.isNotEmpty()) {
+        navController.popBackStack()
+        navController.navigate(NAV_LIST_OF_FILES)
+    }
 
     OnBoardContent(
         uuid = key,
@@ -25,7 +34,9 @@ fun OnBoardScreen(
             viewModel.getKey()
         },
         onKeyClick = {
-
+            viewModel.setSharedKey(it)
+            navController.popBackStack()
+            navController.navigate(NAV_LIST_OF_FILES)
         }
     )
 }
--- android/app/src/main/java/com/bemos/nimbus/presentation/on_board/vm/factory/OnBoardViewModelFactory.kt
+++ android/app/src/main/java/com/bemos/nimbus/presentation/on_board/vm/factory/OnBoardViewModelFactory.kt
@@ -3,14 +3,20 @@ package com.bemos.nimbus.presentation.on_board.vm.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.bemos.nimbus.domain.use_cases.GetKeyUseCase
+import com.bemos.nimbus.domain.use_cases.GetSharedKeyUseCase
+import com.bemos.nimbus.domain.use_cases.SetSharedKeyUseCase
 import com.bemos.nimbus.presentation.on_board.vm.OnBoardViewModel
 
 class OnBoardViewModelFactory(
-    private val getKeyUseCase: GetKeyUseCase
+    private val getKeyUseCase: GetKeyUseCase,
+    private val setSharedKeyUseCase: SetSharedKeyUseCase,
+    private val getSharedKeyUseCase: GetSharedKeyUseCase
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return OnBoardViewModel(
-            getKeyUseCase = getKeyUseCase
+            getKeyUseCase = getKeyUseCase,
+            setSharedKeyUseCase = setSharedKeyUseCase,
+            getSharedKeyUseCase = getSharedKeyUseCase
         ) as T
     }
 }
