d71d335a110504eea52aae5a2d816c1837d5b140
bemos <bemo.0@mail.ru>
2024-12-22 22:02:49 +0300

    - new functions

--- android/app/src/main/java/com/bemos/nimbus/presentation/list_of_files/vm/ListOfFilesViewModel.kt
+++ android/app/src/main/java/com/bemos/nimbus/presentation/list_of_files/vm/ListOfFilesViewModel.kt
@@ -1,10 +1,14 @@
 package com.bemos.nimbus.presentation.list_of_files.vm
 
+import android.content.ContentResolver
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bemos.nimbus.domain.models.FileModel
+import com.bemos.nimbus.domain.use_cases.DeleteFileUseCase
+import com.bemos.nimbus.domain.use_cases.DownloadFileUseCase
 import com.bemos.nimbus.domain.use_cases.GetListFiles
 import com.bemos.nimbus.domain.use_cases.GetSharedKeyUseCase
+import com.bemos.nimbus.domain.use_cases.UploadFileUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -13,23 +17,23 @@ import javax.inject.Inject
 
 class ListOfFilesViewModel @Inject constructor(
     private val getListFiles: GetListFiles,
-    private val getSharedKeyUseCase: GetSharedKeyUseCase
+    private val getSharedKeyUseCase: GetSharedKeyUseCase,
+    private val downloadFileUseCase: DownloadFileUseCase,
+    private val uploadFileUseCase: UploadFileUseCase,
+    private val deleteFileUseCase: DeleteFileUseCase
 ) : ViewModel() {
 
     private val _fileList = MutableStateFlow(listOf<FileModel>())
     val fileList: StateFlow<List<FileModel>> get() = _fileList
 
     private val _sharedKey = MutableStateFlow("")
-    val sharedKey: StateFlow<String> get() = _sharedKey
 
     init {
         getSharedKey()
     }
 
-    fun getListFiles(
-        userFolder: String
-    ) = viewModelScope.launch {
-        val response = getListFiles.execute(userFolder)
+    fun getListFiles() = viewModelScope.launch {
+        val response = getListFiles.execute(_sharedKey.value)
         if (response.isSuccessful) {
             val list = response.body()
             if (list != null) {
@@ -42,6 +46,37 @@ class ListOfFilesViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    fun deleteFile(
+        fileName: String,
+    ) {
+        deleteFileUseCase.execute(
+            userFolder = _sharedKey.value,
+            filename = fileName,
+            onComplete = {
+                getListFiles()
+            }
+        )
+    }
+
+    fun uploadFile(
+        filePath: String,
+    ) {
+        uploadFileUseCase.execute(
+            userFolder = _sharedKey.value,
+            filePath = filePath,
+            onComplete = {
+                getListFiles()
+            }
+        )
+    }
+
+    fun downloadFile(fileName: String) {
+        downloadFileUseCase.execute(
+            _sharedKey.value,
+            fileName
+        )
     }
 
     private fun getSharedKey() {
--- android/app/src/main/java/com/bemos/nimbus/presentation/list_of_files/vm/factory/ListOfFilesViewModelFactory.kt
+++ android/app/src/main/java/com/bemos/nimbus/presentation/list_of_files/vm/factory/ListOfFilesViewModelFactory.kt
@@ -2,18 +2,27 @@ package com.bemos.nimbus.presentation.list_of_files.vm.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bemos.nimbus.domain.use_cases.DeleteFileUseCase
+import com.bemos.nimbus.domain.use_cases.DownloadFileUseCase
 import com.bemos.nimbus.domain.use_cases.GetListFiles
 import com.bemos.nimbus.domain.use_cases.GetSharedKeyUseCase
+import com.bemos.nimbus.domain.use_cases.UploadFileUseCase
 import com.bemos.nimbus.presentation.list_of_files.vm.ListOfFilesViewModel
 
 class ListOfFilesViewModelFactory(
     private val getListFiles: GetListFiles,
-    private val getSharedKeyUseCase: GetSharedKeyUseCase
+    private val getSharedKeyUseCase: GetSharedKeyUseCase,
+    private val downloadFileUseCase: DownloadFileUseCase,
+    private val uploadFileUseCase: UploadFileUseCase,
+    private val deleteFileUseCase: DeleteFileUseCase
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return ListOfFilesViewModel(
             getListFiles = getListFiles,
-            getSharedKeyUseCase = getSharedKeyUseCase
+            getSharedKeyUseCase = getSharedKeyUseCase,
+            downloadFileUseCase = downloadFileUseCase,
+            uploadFileUseCase = uploadFileUseCase,
+            deleteFileUseCase = deleteFileUseCase
         ) as T
     }
 }
