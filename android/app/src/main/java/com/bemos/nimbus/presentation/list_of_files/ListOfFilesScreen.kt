91a5278e6e62159a6bcf81d2193ec0e9a0d2f844
bemos <bemo.0@mail.ru>
2024-12-22 22:00:30 +0300

    - new parametrs, and func

--- android/app/src/main/java/com/bemos/nimbus/presentation/list_of_files/ListOfFilesScreen.kt
+++ android/app/src/main/java/com/bemos/nimbus/presentation/list_of_files/ListOfFilesScreen.kt
@@ -1,10 +1,13 @@
 package com.bemos.nimbus.presentation.list_of_files
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bemos.nimbus.presentation.list_of_files.vm.ListOfFilesViewModel
 import com.bemos.nimbus.presentation.list_of_files.vm.factory.ListOfFilesViewModelFactory
@@ -18,13 +21,26 @@ fun ListOfFilesScreen(
         factory = listOfFilesViewModelFactory
     )
     val listFiles by viewModel.fileList.collectAsState()
-    val key by viewModel.sharedKey.collectAsState()
 
     LaunchedEffect(Unit) {
-        viewModel.getListFiles(key)
+        viewModel.getListFiles()
     }
 
     ListOfFilesContent(
-        fileList = listFiles
+        fileList = listFiles,
+        onDownloadClick = {
+            viewModel.downloadFile(it)
+        },
+        onDeleteClick = { filename ->
+            viewModel.deleteFile(filename)
+        },
+        onRefresh = {
+            viewModel.getListFiles()
+        },
+        selectedFile = { filePath ->
+            viewModel.uploadFile(
+                filePath!!
+            )
+        }
     )
 }
